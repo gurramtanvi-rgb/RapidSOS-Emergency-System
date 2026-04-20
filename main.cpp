a3bbf09e686c814aa0a7f42afd8ff59a37968df4
gurramtanvi-rgb <gurramtanvi@gmail.com>
2026-04-20 11:15:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+// Function declarations
+void addContact();
+void viewContacts();
+
 int main() {
     int choice;
 
@@ -14,20 +18,20 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-            case 1:
-                cout << "Add Contact selected\n";
-                break;
-            case 2:
-                cout << "View Contacts selected\n";
-                break;
-            case 3:
-                cout << "SOS Triggered!\n";
-                break;
-            case 4:
-                cout << "Exiting...\n";
-                return 0;
-            default:
-                cout << "Invalid choice\n";
-        }
+    case 1:
+        addContact();
+        break;
+    case 2:
+        viewContacts();
+        break;
+    case 3:
+        cout << "SOS Triggered!\n";
+        break;
+    case 4:
+        cout << "Exiting...\n";
+        return 0;
+    default:
+        cout << "Invalid choice\n";
+}
     }
 }
