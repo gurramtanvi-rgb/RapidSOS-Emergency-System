ee4a801046599aa7a0177ef7b59f5b059c884e3e
Neha-kanumuri08 <kanumurineha2008@gmail.com>
2026-04-20 11:21:18 +0530

    Update routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+void showSafeRoute() {
+    cout << "\nFinding Safe Route...\n";
+
+    string blocked;
+    cout << "Enter blocked route (example: Market-Hospital): ";
+    cin >> blocked;
+
+    if (blocked == "Market-Hospital") {
+        cout << "Safe Route: Home -> School -> Shelter -> Hospital\n";
+    } else {
+        cout << "Safe Route: Home -> Market -> Hospital\n";
+    }
+}
